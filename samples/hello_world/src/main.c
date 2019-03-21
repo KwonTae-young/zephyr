8262c53f329585dc78e53eb3da090454427220e0
Kwon Tae-young <tykwon@m2i.co.kr>
2019-03-21 15:35:54 +0900

    samples: hello_world: Memory usage test
    source upload

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -7,7 +7,25 @@
 #include <zephyr.h>
 #include <misc/printk.h>
 
+#if defined(CONFIG_BOARD_NRF52840_PCA10056)
+/* [success] SRAM: 99.60% */
+#define SIZE		64000
+#elif defined(CONFIG_BOARD_STM32F4_DISCO)
+/* [fail] SRAM: 83.59% */
+#define SIZE		40000
+
+/* [success] SRAM: 63.25% */
+//#define SIZE		30000
+#else
+#error untested board.
+#endif
+
+static int test[SIZE];
+
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	printk("%p\n", test);
+	printk("sizeof(test) = %d\n", sizeof(test));
 }
