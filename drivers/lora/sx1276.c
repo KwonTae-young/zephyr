05545d2d5b36021283f5f075ef4c8fda0318f40a
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2019-10-28 15:28:46 +0530

    [WIP] Initial LoRaWAN support
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

--- drivers/lora/sx1276.c
+++ drivers/lora/sx1276.c
@@ -25,6 +25,12 @@ LOG_MODULE_REGISTER(sx1276);
 #define SX1276_REG_PA_DAC			0x4d
 #define SX1276_REG_VERSION			0x42
 
+#define BOARD_TCXO_WAKEUP_TIME	5
+
+/* TODO: Use RTC backup */
+static uint32_t backup_reg[2] = { 0 ,0 };
+static uint32_t saved_time;
+
 extern DioIrqHandler *DioIrq[];
 
 int sx1276_dio_pins[SX1276_MAX_DIO] = {
@@ -76,6 +82,11 @@ void SX1276SetBoardTcxo(u8_t state)
 	/* TODO */
 }
 
+uint32_t SX1276GetBoardTcxoWakeupTime(void)
+{
+	return BOARD_TCXO_WAKEUP_TIME;
+}
+
 void SX1276SetAntSw(u8_t opMode)
 {
 	/* TODO */
@@ -104,9 +115,14 @@ void BoardCriticalSectionEnd(uint32_t *mask)
 	irq_unlock(*mask);
 }
 
-uint32_t RtcGetTimerElapsedTime(void)
+uint32_t RtcGetTimerValue(void)
 {
 	return counter_read(dev_data.counter);
+}
+
+uint32_t RtcGetTimerElapsedTime(void)
+{
+	return (uint32_t)(counter_read(dev_data.counter) - saved_time);
 }
 
 u32_t RtcGetMinimumTimeout(void)
@@ -127,17 +143,52 @@ void RtcSetAlarm(uint32_t timeout)
 
 uint32_t RtcSetTimerContext(void)
 {
-	return 0;
+	saved_time = counter_read(dev_data.counter);
+
+	return (uint32_t)saved_time;
+}
+
+uint32_t RtcGetTimerContext(void)
+{
+	return saved_time;
 }
 
 uint32_t RtcMs2Tick(uint32_t milliseconds)
 {
-	return counter_us_to_ticks(dev_data.counter, (milliseconds / 1000));
+	return counter_us_to_ticks(dev_data.counter, (milliseconds * 1000));
+}
+
+uint32_t RtcTick2Ms(uint32_t tick)
+{
+	return (counter_ticks_to_us(dev_data.counter, tick) / 1000);
 }
 
 void DelayMsMcu(uint32_t ms)
 {
 	k_sleep(ms);
+}
+
+uint32_t RtcGetCalendarTime(uint16_t *milliseconds)
+{
+	u32_t time_us;
+
+	time_us = counter_ticks_to_us(dev_data.counter, counter_read(dev_data.counter));
+	*milliseconds = time_us / USEC_PER_MSEC;
+
+	/* Return in seconds */
+	return time_us / USEC_PER_SEC;
+}
+
+void RtcBkupWrite(uint32_t data0, uint32_t data1)
+{
+	backup_reg[0] = data0;
+	backup_reg[1] = data1;
+}
+
+void RtcBkupRead(uint32_t *data0, uint32_t *data1)
+{
+	*data0 = backup_reg[0];
+	*data1 = backup_reg[1];
 }
 
 static void sx1276_irq_callback(struct device *dev,
@@ -406,6 +457,7 @@ const struct Radio_s Radio = {
 	.Random = SX1276Random,
 	.SetRxConfig = SX1276SetRxConfig,
 	.SetTxConfig = SX1276SetTxConfig,
+	.TimeOnAir = SX1276GetTimeOnAir,
 	.Send = SX1276Send,
 	.Sleep = SX1276SetSleep,
 	.Standby = SX1276SetStby,
@@ -415,6 +467,8 @@ const struct Radio_s Radio = {
 	.WriteBuffer = SX1276WriteBuffer,
 	.ReadBuffer = SX1276ReadBuffer,
 	.SetMaxPayloadLength = SX1276SetMaxPayloadLength,
+	.SetPublicNetwork = SX1276SetPublicNetwork,
+	.GetWakeupTime = SX1276GetWakeupTime,
 	.IrqProcess = NULL,
 	.RxBoosted = NULL,
 	.SetRxDutyCycle = NULL,
@@ -476,6 +530,10 @@ static int sx1276_lora_init(struct device *dev)
 		LOG_ERR("Cannot get pointer to %s device", DT_RTC_0_NAME);
 		return -EIO;
 	}
+
+#ifdef CONFIG_LORAWAN
+	counter_start(dev_data.counter);
+#endif
 
 	k_sem_init(&dev_data.data_sem, 0, UINT_MAX);
 
---
+++ include/lorawan.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2019 Manivannan Sadhasivam
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_LORAWAN_H_
+#define ZEPHYR_INCLUDE_LORAWAN_H_
+
+/**
+ * @file
+ * @brief Public LoRaWAN APIs
+ */
+
+#include <zephyr/types.h>
+#include <device.h>
+
+enum lorawan_class {
+	LORAWAN_CLASS_A = 0x00,
+	LORAWAN_CLASS_B = 0x01,
+	LORAWAN_CLASS_C = 0x02,
+};
+
+enum lorawan_act_type {
+	LORAWAN_ACT_OTAA = 0,
+	LORAWAN_ACT_ABP,
+};
+
+enum lorawan_datarate {
+	LORAWAN_DR_0 = 0,
+	LORAWAN_DR_1,
+	LORAWAN_DR_2,
+	LORAWAN_DR_3,
+	LORAWAN_DR_4,
+	LORAWAN_DR_5,
+	LORAWAN_DR_6,
+	LORAWAN_DR_7,
+	LORAWAN_DR_8,
+	LORAWAN_DR_9,
+	LORAWAN_DR_10,
+	LORAWAN_DR_11,
+	LORAWAN_DR_12,
+	LORAWAN_DR_13,
+	LORAWAN_DR_14,
+	LORAWAN_DR_15,
+};
+
+struct lorawan_mib_config {
+	enum lorawan_class lw_class;
+	enum lorawan_datarate rx2_datarate;
+	u32_t rx2_freq;
+	u8_t *dev_eui;
+	u8_t *join_eui;
+	u8_t *app_eui;
+	u8_t *app_key;
+	u8_t *nwk_key;
+	bool adr_enable;
+};
+
+extern int lorawan_config(struct lorawan_mib_config *mib_config);
+extern int lorawan_join_network(enum lorawan_datarate datarate,
+				enum lorawan_act_type mode);
+
+#endif	/* ZEPHYR_INCLUDE_LORAWAN_H_ */
